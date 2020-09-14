4e9ae3d2f590483f7a3dbf30b32945336d0d62a6
黃柏澄(風雨) <wartw@users.noreply.github.com>
2020-09-14 18:38:54 +0800

    Create GuestRoute.php

---
+++ app/Http/Routes/GuestRoute.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Http\Routes;
+
+use Illuminate\Contracts\Routing\Registrar;
+
+class GuestRoute
+{
+    public function map(Registrar $router)
+    {
+        $router->group([
+            'prefix' => 'guest'
+        ], function ($router) {
+            // Plan
+            $router->get ('/plan/fetch', 'Guest\\PlanController@fetch');
+            // Order
+            $router->post('/order/alipayNotify', 'Guest\\OrderController@alipayNotify');
+            $router->post('/order/stripeNotify', 'Guest\\OrderController@stripeNotify');
+            $router->post('/order/bitpayXNotify', 'Guest\\OrderController@bitpayXNotify');
+            $router->post('/order/mgateNotify', 'Guest\\OrderController@mgateNotify');
+            $router->post('/order/epayNotify', 'Guest\\OrderController@epayNotify');
+            $router->post('/order/epayNotify', 'Guest\\OrderController@epayNotify');
+            $router->get('/order/epayNotify', 'Guest\\OrderController@epayNotify');
+            // Telegram
+            $router->post('/telegram/webhook', 'Guest\\TelegramController@webhook');
+        });
+    }
+}
